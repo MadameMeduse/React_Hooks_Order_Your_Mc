d0909b7805febcf403944d62734b27f94726d406
Monika Waligóra <m1waligora@gmail.com>
2020-07-06 17:13:09 +0200

    creatte App component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [shoppingList, setShoppingList] = useState([]);
+
+  const addItemToShoppingList = value => {
+    setShoppingList([...shoppingList, value]);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        {shoppingList.map(item => (
+          <li>{item}</li>
+        ))}
+      </ul>
+      <input name="item" />
+      <button onClick={addItemToShoppingList}>Add item</button>
     </div>
   );
-}
+};
 
 export default App;

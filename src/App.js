9f028fd793b7508c1c91fef0cfb823ae1e176141
Monika Waligóra <m1waligora@gmail.com>
2020-07-06 17:51:40 +0200

    create funtionality for adding items

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
-import logo from "./logo.svg";
+import React, { useState } from "react";
 import "./App.css";
 
 const App = () => {
   const [shoppingList, setShoppingList] = useState([]);
+  const [inputValue, setInputValue] = useState("");
 
   const addItemToShoppingList = value => {
     setShoppingList([...shoppingList, value]);
+    setInputValue("");
+  };
+
+  const handleInputChange = e => {
+    setInputValue(e.target.value);
   };
 
   return (
     <div className="App">
       <ul>
         {shoppingList.map(item => (
-          <li>{item}</li>
+          <li>
+            {item}
+            <button>remove</button>
+          </li>
         ))}
       </ul>
-      <input name="item" />
-      <button onClick={addItemToShoppingList}>Add item</button>
+      <input value={inputValue} onChange={handleInputChange} name="item" />
+      <button onClick={() => addItemToShoppingList(inputValue)}>
+        Add item
+      </button>
     </div>
   );
 };

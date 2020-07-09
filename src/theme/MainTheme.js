aea994d3cc3e97e7e3a42fad824d619468d16ec8
Monika Waligóra <m1waligora@gmail.com>
2020-07-09 16:18:49 +0200

    add extras to the theme

--- src/theme/MainTheme.js
+++ src/theme/MainTheme.js
@@ -1,4 +1,10 @@
 export const theme = {
   shadow: "0 10px 20px -10px hsla(0,0%,0%,0.1)",
-  primaryYellow: "#f8bc0c"
+  primaryYellow: "#f8bc0c",
+  grey100: "hsl(0, 0%, 96%)",
+  grey200: "hsl(0, 0%, 90%)",
+  grey300: "hsl(0, 0%, 70%)",
+  black: "hsl(0, 0%, 0%)",
+  light: 300,
+  bold: 600
 };
